62b6c1376460331eecce12a7dcbd48a5f3294a5c
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-02-01 20:52:26 -0300

    minimap pin initial logic

--- project/Characters/Enemies/WaveControler.gd
+++ project/Characters/Enemies/WaveControler.gd
@@ -1,17 +1,17 @@
-extends YSort
+extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var new_enemies = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Mnimap.load_player($Player)
-	$Minimap.load_wave_controller($WaveController)
 	pass # Replace with function body.
 
+func get_new_enemies():
+	var ne = new_enemies
+	new_enemies.clear()
+	return ne
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- project/HUD/minimap/minimap.gd
+++ project/HUD/minimap/minimap.gd
@@ -1,12 +1,14 @@
 extends CanvasLayer
 
-var buildings
-var current_enemies
+onready var buildings = []
+onready var current_enemies = []
 var player
-
+var wave_controller
 #loaded once
 func load_player(plyer):
 	player = plyer
+func load_wave_controller(controller):
+	wave_controller = controller
 func load_buildings(building_list):
 	buildings = building_list
 
@@ -19,5 +21,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	
+func _physics_process(_delta):
+	var newer = wave_controller.get_new_enemies()
+	for en in newer:
+		$Enemies.add_child($PinGenerator.create_pin("enemy", en))
+	var bag = 
