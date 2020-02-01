628b5e610a9631a51dd9da4b1034b0ff0f8a0aea
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-01 09:48:32 -0300

    Refactor some player code

--- project/Characters/Player/Player.gd
+++ project/Characters/Player/Player.gd
@@ -5,13 +5,14 @@ export(float) var dash_time = 0.15
 export(float) var dash_length = 100
 export(float) var dash_cooldown = 0.7
 
-#var look_angle: float = 0.0
-var attacking := false
-
 onready var can_dash = true
 var dash_direction: Vector2
 var dashing := false
 var dash_speed: float = dash_length / dash_time
+
+
+#var look_angle: float = 0.0
+var attacking := false
 onready var is_invulnerable = false
 
 func _ready():
@@ -24,40 +25,8 @@ func _physics_process(_delta):
 		update_direction_from_input()
 
 	#update_look_angle()
-
-	if self.is_invulnerable:
-		self.modulate.a = 0.4
-	else:
-		self.modulate.a = 1.0
-
-	if Input.is_action_just_pressed("ui_select") and not attacking and can_dash:
-		if Input.is_action_pressed("ui_up") and !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
-			dash_direction = Vector2(0,-1)
-			dash()
-		elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
-			dash_direction = Vector2(-1,-1)
-			dash()
-		elif !Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_down"):
-			dash_direction = Vector2(-1,0)
-			dash()
-		elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down") and !Input.is_action_pressed("ui_right"):
-			dash_direction = Vector2(-1,1)
-			dash()
-		elif !Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down") and !Input.is_action_pressed("ui_right"):
-			dash_direction = Vector2(0,1)
-			dash()
-		elif !Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
-			dash_direction = Vector2(1,1)
-			dash()
-		elif Input.is_action_pressed("ui_right") and !Input.is_action_pressed("ui_down") and !Input.is_action_pressed("ui_up"):
-			dash_direction = Vector2(1,0)
-			dash()
-		elif !Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
-			dash_direction = Vector2(1,-1)
-			dash()
-		else:
-			dash_direction = Vector2(0,1)
-			dash()
+	blink_if_invulnerable()
+	handle_dash_input()
 
 	if dashing:
 		move_and_slide(dash_speed * dash_direction.normalized())
@@ -104,7 +73,43 @@ func update_direction_from_input():
 #  var look_vector = mouse_position - player_position
 #  look_angle = look_vector.angle()
 
-func dash():
+func blink_if_invulnerable():
+	if self.is_invulnerable:
+		self.modulate.a = 0.4
+	else:
+		self.modulate.a = 1.0
+
+func handle_dash_input():
+	if Input.is_action_just_pressed("ui_select") and not attacking and can_dash:
+		if Input.is_action_pressed("ui_up") and !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
+			dash_direction = Vector2(0,-1)
+			setup_dash()
+		elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
+			dash_direction = Vector2(-1,-1)
+			setup_dash()
+		elif !Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_down"):
+			dash_direction = Vector2(-1,0)
+			setup_dash()
+		elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down") and !Input.is_action_pressed("ui_right"):
+			dash_direction = Vector2(-1,1)
+			setup_dash()
+		elif !Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down") and !Input.is_action_pressed("ui_right"):
+			dash_direction = Vector2(0,1)
+			setup_dash()
+		elif !Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
+			dash_direction = Vector2(1,1)
+			setup_dash()
+		elif Input.is_action_pressed("ui_right") and !Input.is_action_pressed("ui_down") and !Input.is_action_pressed("ui_up"):
+			dash_direction = Vector2(1,0)
+			setup_dash()
+		elif !Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
+			dash_direction = Vector2(1,-1)
+			setup_dash()
+		else:
+			dash_direction = Vector2(0,1)
+			setup_dash()
+
+func setup_dash():
 	#$Sprite/SFXDash.pitch_scale = randf() + 1.0
 	#$Sprite/SFXDash.play()
 	dashing = true
