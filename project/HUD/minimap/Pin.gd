5aee6f67da9ee1c51f64f29d558c60515549724f
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-02-01 22:37:14 -0300

    finished testing player pin

--- project/HUD/minimap/Pin.gd
+++ project/HUD/minimap/Pin.gd
@@ -3,15 +3,17 @@ class_name Pin
 
 var ref_object
 var pos #(0->100, 0->100) [%]
-onready var disp_pos = [0,0]
+var disp_pos
 
 func update_state():
 	pass
 	
 func set_obj(obj):
 	ref_object = obj
+	disp_pos=[0,0]
 
 func set_disp_pos():
-	disp_pos[0] = pos.x * global.minimap_area[2]
-	disp_pos[1] = pos.y * global.minimap_area[3]
-	position = disp_pos
+	disp_pos[0] = pos[0] * global.minimap_area[2]
+	disp_pos[1] = pos[1] * global.minimap_area[3]
+	position.x = disp_pos[0] + 2
+	position.y = disp_pos[1] - 2
