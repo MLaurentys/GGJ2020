b6570c8926c19199e9d21a5dfcf7b7abc094b7c5
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-02 01:43:22 -0300

    Add monster spawner

---
+++ project/Characters/Enemies/SpawnTimer.gd
@@ -0,0 +1,13 @@
+extends Timer
+
+const MONSTER_SCENE = preload("res://Characters/Enemies/Alien.tscn")
+
+var monsters_to_spawn
+
+func _on_SpawnTimer_timeout():
+	var monster = MONSTER_SCENE.instance()
+	get_parent().get_parent().add_child(monster)
+
+	monsters_to_spawn -= 1
+	if (monsters_to_spawn == 0):
+		self.queue_free()
