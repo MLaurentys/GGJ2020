80691f8b7a5ff02df860da4d7e6712d2a6fa8b98
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-02 15:05:11 -0300

    Now when the player dies, the game over menu appears

--- project/HUD/RetryButton.gd
+++ project/HUD/RetryButton.gd
@@ -3,4 +3,4 @@ extends Button
 func _on_Retry_pressed() -> void:
 	$Effect.play()
 	var tree = self.get_tree()
-	var error_flag = tree.change_scene("res://Game.tscn")
+	tree.change_scene("res://Game.tscn")
